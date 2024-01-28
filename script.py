42f0e331028a6ccf03a430eb20eade4d42be43fb
minuma <tamihuru75@gmail.com>
2024-01-29 02:25:19 +0900

    Add result.txt to .gitignore and modify script.py to append command results to result.txt

--- script.py
+++ script.py
@@ -2,13 +2,22 @@ import schedule
 import time
 import subprocess
 
-
 def job():
-      subprocess.run(['python', '/home/minuma/free-bot/lstm/historical/syve_api.py'], capture_output=True, text=True)
-      subprocess.run(['python', '/home/minuma/free-bot/lstm/gbm_predict.py'], capture_output=True, text=True)
-      subprocess.run(['python', '/home/minuma/free-bot/bot/gateio.py'], capture_output=True, text=True)
+    with open('/home/minuma/free-bot/result.txt', 'a') as f:  # ここで指定したパスに結果を追記します
+        # 各コマンドの実行結果をファイルに書き込む
+        result = subprocess.run(['python', '/home/minuma/free-bot/lstm/historical/syve_api.py'], capture_output=True, text=True)
+        f.write(result.stdout + "\n")
 
-# 1分ごとにjobを実行
+        result = subprocess.run(['python', '/home/minuma/free-bot/lstm/gbm_predict.py'], capture_output=True, text=True)
+        f.write(result.stdout + "\n")
+
+        result = subprocess.run(['python', '/home/minuma/free-bot/bot/gateio.py'], capture_output=True, text=True)
+        f.write(result.stdout + "\n")
+
+# スケジュール設定前に1回実行
+job()
+
+# 10分ごとにjobを実行
 schedule.every(10).minutes.do(job)
 
 while True:
