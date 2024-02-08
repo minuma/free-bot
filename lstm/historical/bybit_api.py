cddb2283c92111a09e496ec2ce325368eaf62bc8
minuma <tamihuru75@gmail.com>
2024-02-08 10:44:28 +0900

    Update start and end dates in bybit_api.py

--- lstm/historical/bybit_api.py
+++ lstm/historical/bybit_api.py
@@ -10,15 +10,15 @@ category = "linear"
 interval = 15
 
 # 特定の開始日を設定（例：2023年1月1日）
-start_date_str = "2023-01-01"
+start_date_str = "2024-01-01"
 start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
 
 # 1ヶ月後の日付を計算
-end_date = start_date + timedelta(days=2)
+end_date = start_date + timedelta(days=30)
 
 # UNIXタイムスタンプに変換
-start_timestamp = int(start_date.timestamp())
-end_timestamp = int(end_date.timestamp())
+start_timestamp = int(start_date.timestamp()) * 1000
+end_timestamp = int(end_date.timestamp()) * 1000
 
 values = []
 
@@ -29,8 +29,8 @@ while current_start_timestamp < end_timestamp:
         "symbol": symbol,
         "interval": interval,
         "category": category,
-        "start": current_start_timestamp * 1000,
-        "end": min((current_start_timestamp + 200 * 60 * interval) * 1000, end_timestamp * 1000),
+        "start": current_start_timestamp,
+        "end": min((current_start_timestamp + 200 * 60 * interval * 1000), end_timestamp),
         "limit": 200
     }
 
@@ -40,10 +40,14 @@ while current_start_timestamp < end_timestamp:
     if len(response_data["result"]["list"]) == 0:
         break
 
-    values += response_data["result"]["list"]
+    original_list = response_data["result"]["list"]
+    reversed_list = original_list[::-1]
+    values += reversed_list
     # 最後のデータのタイムスタンプを新しい開始点として設定
     last_timestamp = int(values[-1][0]) 
+    # print(values[-1])
     current_start_timestamp = last_timestamp
+    print(current_start_timestamp)
 
 data = pd.DataFrame(values)
 
@@ -66,4 +70,6 @@ data.sort_values("date_close", inplace=True)
 # # "datetime"をインデックスに設定
 # data.set_index("datetime", inplace=True)
 
-data.to_csv(f"bybit_{symbol}_{category}_{interval}_{start_date_str}_to_{end_date.strftime('%Y-%m-%d')}.csv", index=False)
+new_date_str = datetime.strptime(start_date_str, "%Y-%m-%d").strftime("%Y%m%d")
+# data.to_csv(f"./lstm/historical/bybit_{symbol}_{category}_{interval}_{start_date_str}_to_{end_date.strftime('%Y-%m-%d')}.csv", index=False)
+data.to_csv(f"./lstm/historical/historical_price_{new_date_str}.csv", index=False)
